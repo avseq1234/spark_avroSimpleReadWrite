3946cb963ea87a822fdf02cd651e5176864c4e78
avseq1234 <sun.stef@gmail.com>
2017-11-07 17:04:35 +0800

    initial version

---
+++ build.sbt
@@ -0,0 +1,16 @@
+name := "spark_avroSimpleReadWrite"
+
+version := "1.0"
+
+scalaVersion := "2.10.4"
+
+libraryDependencies += "com.databricks" % "spark-avro_2.10" % "2.0.1"
+
+libraryDependencies += "org.apache.spark" % "spark-core_2.10" % "1.6.2"
+
+libraryDependencies += "org.apache.spark" % "spark-sql_2.10" % "1.6.2"
+
+
+
+
+    
---
+++ src/main/scala/com/test/SimpleAvroReadWrite.scala
@@ -0,0 +1,32 @@
+package com.test
+
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.{SparkConf, SparkContext}
+import com.databricks.spark.avro._
+
+
+/**
+  * Created by Honda on 2017/11/7.
+  */
+object SimpleAvroReadWrite {
+  def main(args: Array[String]): Unit = {
+    val sparkConf = new SparkConf().setAppName("Spark Avro")
+    val sc = new SparkContext(sparkConf)
+    val sqlContext = new SQLContext(sc)
+    import sqlContext.implicits._
+
+    val df = Seq(
+      (2012,8,"Batman",9.8),
+      (2012,8,"Hero" , 8.7),
+      (2011,7,"Git",2.0),
+      (2011,6,"Wonder Women" ,10)
+    ).toDF("year","month","title","rating")
+
+    df.coalesce(1).write.avro("avrotest")
+
+    val dfReader = sqlContext.read.avro("avrotest/*.avro")
+    dfReader.filter("rating > 5").foreach(println)
+
+
+  }
+}
